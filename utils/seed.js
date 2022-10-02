cac6ed29004da8fc29dd0a9e4b6dda3b6631b7d1
Dragon Lore <tate.j.foster@gmail.com>
2022-10-02 08:58:12 -0600

    setting up seed data need to get Thought _id values into thoughts array in Users

--- utils/seed.js
+++ utils/seed.js
@@ -1,1 +1,37 @@
 const connection = require("../config/connection");
+const { Users, Thoughts, Reactions } = require("../models");
+const { users, getRandomThoughts } = require("./data");
+
+connection.on("error", (err) => err);
+
+connection.once("open", async () => {
+	console.log("connected");
+	await Thoughts.deleteMany({});
+	await Users.deleteMany({});
+
+	const thoughts = getRandomThoughts(20);
+
+	await Thoughts.collection.insertMany(thoughts);
+	await Users.collection.insertMany(users);
+
+	const createdThoughts = await Thoughts.find();
+	console.log(createdThoughts);
+	const updateUsers = () => {
+		for (let i = 0; i < createdThoughts.length; i++) {
+			if (createdThoughts[i].username === users.username) {
+				Users.findOneAndUpdate(
+					{ username: createdThoughts[i].username },
+					{ $addToSet: { thoughts: createdThoughts[i]._id } },
+					{ new: true }
+				);
+			}
+			return;
+		}
+	};
+
+	updateUsers();
+	console.table(users);
+	console.table(thoughts);
+	console.info("Seeding complete! 🌱");
+	process.exit(0);
+});
